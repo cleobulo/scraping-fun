9d4ca0f42caecf839a1dab9f2d3ad2231b817584
Cleóbulo Oliveira <cleobulo.oliveira@itriad.org.br>
2024-12-11 10:04:50 -0400

    feat: use lxml html parser

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import re
 import requests
 
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit
 
 BASE_URL = 'https://python.org'
 NEXT_URL = ''
@@ -15,16 +15,18 @@ def download_page(url):
     
     return page.text
 
-def extract_internal_links(html):
-    soup = BeautifulSoup(html, 'html.parser')
+def extract_internal_links(html, next_url):
+    soup = BeautifulSoup(html, 'lxml')
     links = soup.find_all('a')
+    split_next_url = urlsplit(next_url)
+    base_next_url = f'{split_next_url.scheme}://{split_next_url.netloc}'
     
     for link in links:
         if 'href' in link.attrs:
             href = link.attrs['href']
 
             if re.match('^/\w+', href):
-                new_url = urljoin(BASE_URL, href)
+                new_url = urljoin(base_next_url, href)
                 
                 if new_url not in DONE_URL_LIST:
                     TODO_URL_LIST.add(new_url)
@@ -33,7 +35,7 @@ def main():
     NEXT_URL = TODO_URL_LIST.pop()
     while NEXT_URL not in DONE_URL_LIST:
         html = download_page(NEXT_URL)
-        extract_internal_links(html)
+        extract_internal_links(html, NEXT_URL)
         print(NEXT_URL)
         NEXT_URL = TODO_URL_LIST.pop()
 try:
